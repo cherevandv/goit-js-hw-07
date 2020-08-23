fcf8ddfd2d2281e10372cfb7e15b4dc842ec49a9
Daria Kobekhno <cherevandv@gmail.com>
2020-08-23 18:52:52 +0300

    added task-01

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const list = document.querySelector('#categories');
+const categories = list.querySelectorAll('.item');
+
+console.log(`В списке ${categories.length} категории.`);
+
+const arrayOfCategories = Array.from(categories);  
+
+const showInfo = arrayOfCategories.forEach(category => {
+        console.log(`Категория: ${category.firstElementChild.textContent}`);
+        const listItems = (category.querySelector('ul'));
+        console.log(`Количество элементов: ${listItems.children.length}`);
+    });
